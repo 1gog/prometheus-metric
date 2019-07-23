96f397c95ba9291c61d5f4cd368e62c6605f206c
Anatoliy Mirzuev <mirzuev.a.v@gmail.com>
2019-07-23 22:18:22 +0000

    .

--- kafka_cpu.py
+++ kafka_cpu.py
@@ -2,49 +2,27 @@
 
 
 from prometheus_client import start_http_server, Summary, Counter, Gauge
-# import time
 import random
 from time import time, sleep
+import os
 
-import subprocess
-subprocess.run(["ls", "-l"])
+def getValue():
+        cpu_mem_metric = os.popen('ps -heo %cpu,%mem -p 1').read()
 
+        raw = cpu_mem_metric.rstrip().split(' ')
 
-# example for Summary
-#REQUEST_TIME = Summary('cpu_kafka', 'Time spent processing request')
-
-
-# example for Counter
-#c = Counter('kafka_cpu', 'Kafka Cpu usage')
-# example for Gauges
-
-g = Gauge('g_cpu_kafka','The gauge cpu usage')
+        (cpu,mem) = list(filter(lambda x: x != '', raw))
+        return ( cpu, mem )
 
 
-#@REQUEST_TIME.time()
-#def process_request(t):
-#    """A dummy function that takes some time."""
-#    time.sleep(t)
-
-#def cpu_count():
-#    return c.inc(random.random())
-
-@g.track_inprogress()
-def gauge_kafka_cpu():
-    #g1 = Gauge('g_cpu_kafka','The gauge cpu usage')
-    g.set(random.randrange(1,100,1))
-
-start_http_server(8181)
-while True:
-    #g1 = Gauge('g_cpu_kafka','The gauge cpu usage')
-    #g1.set(random.randrange(1,100,1))
-    gauge_kafka_cpu()
-    sleep(1)
+kafka_cpu = Gauge('cpu_kafka', 'The gauge cpu usage')
+kafka_mem = Gauge('mem_kafka', 'The gauge memory usage')
 
 
-
-#if __name__ == "__main__":
-#    start_http_server(8181)
-#    while True:
-#       #process_request(1)
-#       gauge_kafka_cpu()
+if __name__ == "__main__":
+    start_http_server(8181)
+    while True:
+    	(cpu,mem) = getValue()
+    	kafka_cpu.set( cpu )
+    	kafka_mem.set( mem )
+    	sleep(1)
